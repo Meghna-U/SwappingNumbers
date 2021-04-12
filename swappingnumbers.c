1572b19be22c118110fb8ebab3188746f5186ddc
Meghna-U <82393429+Meghna-U@users.noreply.github.com>
2021-04-12 20:25:58 +0530

    Update swappingnumbers.c

--- swappingnumbers.c
+++ swappingnumbers.c
@@ -4,7 +4,7 @@ void main()
   int a,b;
   printf("Enter two numbers:");
   scanf("%d%d",&a,&b);
-  printf("Before Swapping: a=%d  b=%d",a,b);
+  printf("Before Swapping: a=%d  b=%d\n",a,b);
   a=a+b;
   b=a-b;
   a=a-b;
